03cdc5edd9605bdab201ccdf6c0d9e252f3e20dc
Milind Mishra <milind.mishra4@gmail.com>
2023-04-30 18:31:04 +0800

    added starter files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const { ServerConfig } = require("./config");
+const apiRoutes = require("./routes");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use("/api", apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+  console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+});
