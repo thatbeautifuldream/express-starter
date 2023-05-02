5b2a5f53d3a2b6666cca2ae2e969c8a18e3e352f
Milind Mishra <milind.mishra4@gmail.com>
2023-05-03 00:59:23 +0800

    📦 NEW: setup

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const apiRoutes = require("./routes");
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
